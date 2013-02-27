fab6a968d88a59103fabffd2b5f5a4a4a6f336e1
Andrew <andrew@lobos.me>
2013-02-27 09:06:37 -0500

    Added client management

---
+++ ClientRegistry.js
@@ -0,0 +1,106 @@
+clientList = new Array();
+accessLevels = new Array();
+
+function AccessLevel(level, friendlyName)
+{
+	this.accessList = new Array();
+	this.allowUnknown = false;
+	this.level = level;
+	this.friendlyName = friendlyName;
+	this.isAbleToAccess = function(url)
+	{
+		for ( i = 0; i < this.accessList.length; i++ )
+		{
+			if ( this.accessList[i].search(url) )
+				return !this.allowUnknown;
+		}
+		return this.allowUnknown;
+	}
+	
+	this.addURL = function(regex)
+	{
+		this.accessList.push(url);
+	}
+	
+	this.setAllowUnknown = function(state)
+	{
+		this.allowUnknown = state;
+	}
+	
+	this.getLevel = function()
+	{
+		return this.level;
+	}
+}
+accessLevels.push(new AccessLevel(0, "Unauthenticated"));
+
+function getAccessLevel(level)
+{
+	for ( i = 0; i < accessLevels.length; i++ )
+	{
+		if ( accessLevels[i].getLevel() == level )
+			return accessLevels[i];
+	}
+	return accessLevels[0];
+}
+
+function addAccessLevel(level)
+{
+	level = new AccessLevel(level);
+	accessLevels.push(level);
+}
+
+
+function Client(IP, accessLevel, expire)
+{
+	console.log("New client! IP: "+IP);
+	
+	if ( accessLevel == undefined )
+		accessLevel = 0;
+	this.myAccessLevel = getAccessLevel(accessLevel);
+	this.IP = IP;
+	
+	if ( expire == undefined )
+		expire = 600000; // 10 min
+		
+		
+	thisPtr = this;
+	this.expireTimeout = setTimeout(function()
+	{
+		thisPtr.changeAccessLevel(0);
+	}, expire);
+	
+	this.changeAccessLevel = function(level)
+	{
+		this.myAccessLevel = getAccessLevel(level);
+		console.log("Access level of "+this.getIP+" is now at "+level);
+	}
+	
+	this.handleRequest = function(URL)
+	{
+		return this.myAccessLevel.isAbleToAccess(URL);
+	}
+	
+	this.getAccessLevel = function()
+	{
+		return this.myAccessLevel.getLevel();
+	}
+	
+	this.getIP = function()
+	{
+		return this.IP;
+	}
+}
+
+function getClient(IP)
+{
+	for ( i = 0; i < clientList.length; i++ )
+	{
+		if ( clientList.getIP() == IP )
+			return clientList[i];
+	}
+	client = new Client(IP, 0);
+	clientList.push(client);
+	return client;
+}
+
--- server.js
+++ server.js
@@ -1,22 +1,9 @@
-var http = require('http');
+var proxy = require("./Proxy");
+var clientRegistry = require("./ClientRegistry");
+var express = require("express");
 
-http.createServer(function(request, response) {
-  var proxy = http.createClient(80, request.headers['host'])
-  var proxy_request = proxy.request(request.method, request.url, request.headers);
-  console.log(request.url+" for "+request.connection.remoteAddress);
-  proxy_request.addListener('response', function (proxy_response) {
-    proxy_response.addListener('data', function(chunk) {
-      response.write(chunk, 'binary');
-    });
-    proxy_response.addListener('end', function() {
-      response.end();
-    });
-    response.writeHead(proxy_response.statusCode, proxy_response.headers);
-  });
-  request.addListener('data', function(chunk) {
-    proxy_request.write(chunk, 'binary');
-  });
-  request.addListener('end', function() {
-    proxy_request.end();
-  });
-}).listen(8080);
+
+proxy.init(function(IP, URL)
+{
+	clientRegistry.getClient(IP).handleRequest(URL);
+});
